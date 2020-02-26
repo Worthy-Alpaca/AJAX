52e94ede192b3360892197bf2173abe54fa3dab9
Stephan Schumacher <stephanxxschumacher@gmail.com>
2020-02-27 00:20:27 +0100

    added command modules

--- index.js
+++ index.js
@@ -20,54 +20,13 @@ for (const file of commandFiles) {
 client.once('ready', () => {
     console.log('Ready!');
 });
-////require the discord.js module
-////const config = require('./config.json');
-//const Discord = require('discord.js');
-//const { prefix, token } = require('./config.json');
 
-////create a new Discord client
-//const client = new Discord.Client();
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-////when the client is ready, run this code
-////this event will only trigger one time after logging in
-//client.once('ready', () => {
-//    console.log('Ready!');
-//});
-
-//client.on('message', message => {
-//    if (message.content === `${prefix}about`) {
-//        //send back "Pong" to the channel the message was sent in
-//        message.channel.send(`This bot is still in it's pre-alpha phase. Please report bugs or ideas to Worthy Alpaca`);
-//    } else if (message.content === `${prefix}server`) {
-//        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-//    } else if (message.content === `${prefix}user-info`) {
-//        message.channel.send(`Your Username: ${message.author.username}\nYour ID: ${message.author.id}`);
-//    }
-//});
-
-//client.on('message', message => {
-//    if (!message.content.startsWith(prefix) || message.author.bot) return ;
-
-//    const args = message.content.slice(prefix.length).split(/ +/);
-//    const command = args.shift().toLowerCase();
-
-//    if (command === 'args-info') {
-//        if (!args.length) {
-//            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-//        } else if (args[0] === 'foo') {
-//            return message.channel.send('bar');
-//        } else if (command === 'kick') {
-//            //grab the "first" mentioned user from the message
-//            //this will return a `User` object, just like `message.author`
-//            const taggedUser = message.mentions.users.first();
-
-//            message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-//        }
-
-//        //message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-//        message.channel.send(`First argument: ${args[0]}`);
-//    }
-//})
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+})
 
 
 //login to Discord with the bot's token
