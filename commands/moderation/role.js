20ca7ed50d3622d5fe7476ab118b103ef46a9d50
Stephan Schumacher <stephanxxschumacher@gmail.com>
2020-03-18 01:42:50 +0100

    improved the !role checks

--- commands/moderation/role.js
+++ commands/moderation/role.js
@@ -4,14 +4,34 @@ module.exports = {
     name: "role",
     category: "moderation",
     description: "adds/removes roles",
-    usage: "<add/remove, id | mention>",
+    usage: "<add/remove, id | mention, role>",
     run: async (client, message, args) => {
 
-        if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry pal, you can't do that.");
+
+        //if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry pal, you can't do that.");
+        //let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[1]);
+        //if (!rMember) return message.reply("Should I add or remove the role?");
+        //let role = message.guild.roles.find(r => r.name === args[2]) || message.guild.roles.find(r => r.id === args[2]);
+        //if (!role) return message.reply("Specify a role");
+
+        if (message.deletable) message.delete();
+
+        if (!args[0]) {
+            return message.reply("Please tell me what to do.")
+                .then(m => m.delete(5000));
+        }
+
+        if (!args[1]) {
+            return message.reply("You need to tag someone.")
+                .then(m => m.delete(5000));
+        }
         let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[1]);
-        if (!rMember) return message.reply("Couldn't find that user.");
+
+        if (!args[2]) {
+            return message.reply("You need to specify a role.")
+                .then(m => m.delete(5000));
+        }
         let role = message.guild.roles.find(r => r.name === args[2]) || message.guild.roles.find(r => r.id === args[2]);
-        if (!role) return message.reply("Specify a role");
 
         if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply("I don't have permission to do that. Go fix it!!");
 
@@ -22,7 +42,8 @@ module.exports = {
             await rMember.removeRole(role.id).catch(e => console.log(e.message))
             message.channel.send(`This role, ${role.name}, has been removed from ${rMember}.`)
         } else if ((args[0] !== "add") || (args[0] !== "remove")) {
-            return message.reply("You need to specify what you want me to do.")
+            return message.reply("you did not provide a valid action.")
+                .then(m => m.delete(5000));
         } 
 
 
