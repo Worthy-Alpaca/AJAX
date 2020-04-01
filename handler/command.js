2c1c292b28f75f3c940d59e240727ba36c639b92
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-04-01 14:52:01 +0200

    fixed annotations

--- handler/command.js
+++ handler/command.js
@@ -9,12 +9,12 @@ table.setHeading("Command", "Load status");
 module.exports = (client) => {
     // Read every commands subfolder
     readdirSync("./commands/").forEach(dir => {
-        // Filter so we only have .js command files
+        // Filter for .js command files
         const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
     
         // Loop over the commands, and add all of them to a collection
         // If there's no name found, prevent it from returning an error,
-        // By using a cross in the table we made.
+        // By using a cross in the table.
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
     
