ee2c3fe82e3bd60890a0795c3a56fbcdd6442e68
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-04-06 14:24:15 +0200

    impelemented a PM service into !report

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -6,11 +6,14 @@ module.exports = {
     name: "report",
     category: "moderation",
     description: "reports a member",
-    usage: "<mention | id>",
+    usage: "<good/bad, mention | id, reason>",
     run: async (client, message, args) => {
         if (message.deletable) message.delete();
 
         let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+        let behavior;
+        let behavior2;
 
         if (!rMember)
             return message.reply("Couldn't find that person").then(m => m.delete(5000));
@@ -18,7 +21,17 @@ module.exports = {
         if (rMember.hasPermission("ADMINISTRATOR") || rMember.user.bot)
             return message.reply("Can't report that member").then(m => m.delete(5000));
 
-        if (!args[1])
+        if (args[0] === "good") {
+            behavior = "Keep up the good work!"
+            behavior2 = "good"
+        } else if (args[0] === "bad") {
+            behavior = "Please cease this behavior immediatly. If you think this is wrong, please contact a staff member."
+            behavior2 = "bad"
+        } else if ((args[0] !== "good") || (args[0] !== "bad")) {
+            return message.reply("You need to add a behavior type. (Good/Bad)").then(m => m.delete(5000));
+        }
+
+        if (!args[2])
             return message.channel.send("Please include a reason for the report").then(m => m.delete(10000));
 
         const channel = message.guild.channels.find(channel => channel.name === "reports");
@@ -32,10 +45,17 @@ module.exports = {
             .setFooter(message.guild.name, message.guild.iconURL)
             .setAuthor("Reported member", rMember.user.displayAvatarURL)
             .setDescription(stripIndents`**> Member: ${rMember} (${rMember.id})
+            **> Behavior: ${behavior2}
             **> Reported by: ${message.member} (${message.member.id})
             **> Reported in: ${message.channel}
-            **> Reason: ${args.slice(1).join(" ")}`);
+            **> Reason: ${args.slice(2).join(" ")}`);
+
+        client.fetchUser(`${rMember.id}`, false).then(user => {
+        user.send(`You have been reported by ${message.member} for "${args.slice(2).join(" ")}." ${behavior} This message was computer generated. Please do not answer to it.`)
+        });
 
         return channel.send(embed);
+
+        
     }
 }
