00a9bd8151874b82971c82b4150dfdd67974b83d
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-04-24 15:33:44 +0200

    started working on edsm API to find system information

--- commands/fun/elite.js
+++ commands/fun/elite.js
@@ -1,4 +1,8 @@
-
+const Discord = require("discord.js");
+const superagent = require("superagent");
+const querystring = require('querystring');
+const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+const { RichEmbed } = require("discord.js");
 
 
 module.exports = {
@@ -7,8 +11,54 @@ module.exports = {
     description: "Don't use me. I'm not done.",
     usage: "don't use me yet!",
     run: async (client, message, args) => {
-        if (message.deletable) message.delete();
+        if (message.deletabe) message.delete();
+        return message.reply("This is not working yet. Come back at a later time, maybe I'll have it figured out then :grin:")
 
-        return message.reply("At this point I don't do anything. Come back at a later time :) ");
+        if (!args.length) {
+            return message.channel.send('You need to supply a search term!')
+                .then(m => m.delete(5000));
+        }
+        
+        const query = querystring.stringify({ term: args.join(' ') });
+
+        let {list} = await superagent
+        .get(`https://www.edsm.net/api-v1/system`, {
+            method: `GET`,
+            headers: {
+                systemName: `${args[0]}`,
+                showID: 0,
+                showCoordinates: 0,
+                showPermit: 1,
+                showInformation: 1,
+                showPrimaryStar: 1,
+                includeHidden: 0
+            }
+        })
+        .then(res => console.log(res))
+        .then(json => {
+            console.log('res', json);
+        }).catch(err => {
+            console.error(err);
+            return ReE(res, err.message, 500);
+        });
+        
+
+        if (!list.length) {
+            return message.channel.send(`No results found for **${args.join(' ')}**.`)
+                .then(m => m.delete(5000));
+        }
+
+        const [answer] = list;
+
+        const embed = new RichEmbed()
+            .setColor('RANDOM')
+            .setTitle(answer.word)
+            .setURL(answer.permalink)
+            .addField('Definition', trim(answer.name, 1024) );
+            
+
+        message.channel.send(embed);
+
+        
     }
    }
