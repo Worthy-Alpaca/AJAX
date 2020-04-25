c0d24780008081c9a3bf5001ca5187b4a3741dd0
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-04-25 22:43:51 +0200

    messing around with !elite. Still not working

--- commands/fun/elite.js
+++ commands/fun/elite.js
@@ -3,6 +3,9 @@ const superagent = require("superagent");
 const querystring = require('querystring');
 const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
 const { RichEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const url = "https://www.edsm.net/api-v1/system";
 
 
 module.exports = {
@@ -12,53 +15,53 @@ module.exports = {
     usage: "don't use me yet!",
     run: async (client, message, args) => {
         if (message.deletabe) message.delete();
-        return message.reply("This is not working yet. Come back at a later time, maybe I'll have it figured out then :grin:")
-
+        //return message.reply("This is not working yet. Come back at a later time, maybe I'll have it figured out then :grin:")
+         
         if (!args.length) {
             return message.channel.send('You need to supply a search term!')
                 .then(m => m.delete(5000));
         }
+        const cmdr = args.slice(0).join(" ")
         
-        const query = querystring.stringify({ term: args.join(' ') });
-
-        let {list} = await superagent
-        .get(`https://www.edsm.net/api-v1/system`, {
-            method: `GET`,
-            headers: {
-                systemName: `${args[0]}`,
-                showID: 0,
-                showCoordinates: 0,
-                showPermit: 1,
-                showInformation: 1,
-                showPrimaryStar: 1,
-                includeHidden: 0
-            }
-        })
-        .then(res => console.log(res))
-        .then(json => {
-            console.log('res', json);
-        }).catch(err => {
-            console.error(err);
-            return ReE(res, err.message, 500);
+        /* const response = await fetch('https://inara.cz/inapi/v1/', {
+            method: 'POST',
+            "headers": {
+                "appName": "MyInaraApp",
+                "appVersion": "1.01",
+                "isDeveloped": true,
+                "APIkey": "bckzq90dgzwo444k44g0gss08csoks4w84ogw8k",
+                "commanderName": "Worthy Alpaca",
+                "commanderFrontierID": "3298136"
+            },
+            "events": [
+                {
+                    "systemName": `${cmdr}`
+                    
+                },
+                {
+                    "showInformation": 1
+                }
+            ]
         });
+        return response.json()
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err);
+        }) */
+
         
 
-        if (!list.length) {
-            return message.channel.send(`No results found for **${args.join(' ')}**.`)
-                .then(m => m.delete(5000));
-        }
-
-        const [answer] = list;
-
-        const embed = new RichEmbed()
+        /* const embed = new RichEmbed()
             .setColor('RANDOM')
-            .setTitle(answer.word)
-            .setURL(answer.permalink)
             .addField('Definition', trim(answer.name, 1024) );
             
 
-        message.channel.send(embed);
-
+        message.channel.send(embed); */
+        
+        
         
     }
    }
+
