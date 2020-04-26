84999e684715f44442042d0673195bd8446ce469
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-04-27 00:44:25 +0200

    Got the EDSM API working

--- commands/fun/elite.js
+++ commands/fun/elite.js
@@ -1,67 +1,99 @@
 const Discord = require("discord.js");
-const superagent = require("superagent");
-const querystring = require('querystring');
-const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
 const { RichEmbed } = require("discord.js");
 const fetch = require('node-fetch');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const url = "https://www.edsm.net/api-v1/system";
+const { stripIndents } = require("common-tags");
+
 
 
 module.exports = {
     name: "elite",
     category: "fun",
-    description: "Don't use me. I'm not done.",
-    usage: "don't use me yet!",
+    description: "Displays information about a system",
+    usage: "<System>",
     run: async (client, message, args) => {
         if (message.deletabe) message.delete();
-        return message.reply("This is not working yet. Come back at a later time, maybe I'll have it figured out then :grin:")
-         
+        
         if (!args.length) {
             return message.channel.send('You need to supply a search term!')
                 .then(m => m.delete(5000));
         }
-        const cmdr = args.slice(0).join(" ")
+        const sys = args.slice(0).join("+")
+        const url = `https://www.edsm.net/api-system-v1/bodies?sysname=${sys}`
+        const url2 = `https://www.edsm.net/api-system-v1/stations?sysname=${sys}`
         
-        /* const response = await fetch('https://inara.cz/inapi/v1/', {
-            method: 'POST',
-            "headers": {
-                "appName": "MyInaraApp",
-                "appVersion": "1.01",
-                "isDeveloped": true,
-                "APIkey": "bckzq90dgzwo444k44g0gss08csoks4w84ogw8k",
-                "commanderName": "Worthy Alpaca",
-                "commanderFrontierID": "3298136"
-            },
-            "events": [
-                {
-                    "systemName": `${cmdr}`
-                    
-                },
-                {
-                    "showInformation": 1
+        //this works, don't delete it
+        fetch(url).then(response => response.json()).then(function(response) {
+            
+            var landables = [];
+            var nonlandables = [];
+            var id;
+
+            response.bodies.forEach(function(bodies) {
+                if (bodies.isLandable) {
+                    id = bodies.id
+                    landables.push(id)
+                } else if (!bodies.isLandable) {
+                    id = bodies.id
+                    nonlandables.push(id)
                 }
-            ]
-        });
-        return response.json()
-        .then(data => {
-            console.log(data)
-        })
-        .catch(err => {
-            console.log(err);
-        }) */
+            })
 
-        
-
-        /* const embed = new RichEmbed()
+            
+            const embed = new RichEmbed()
             .setColor('RANDOM')
-            .addField('Definition', trim(answer.name, 1024) );
+            .setFooter('EDSM Database')
+            .setTimestamp()
+            .setURL(response.url)
+            .addField('Name', `**${response.name}**` )
+            .addField(`\u200b`,  stripIndents`**Main Star** 
+            Star Class: ${response.bodies[0].subType}
+            Scoopable: ${response.bodies[0].isScoopable}`, true)
+            .addField(`\u200b`, stripIndents`**Planetary Bodies**
+            Landable Bodies: ${landables.length}
+            Nonlandable Bodies: ${nonlandables.length}`, true);
             
 
-        message.channel.send(embed); */
-        
-        
-        
+            message.channel.send(embed);
+        })
+
+        fetch(url2).then(response => response.json()).then(function(response) {
+            var station = [];
+            var outposts = [];
+            var planetary = [];
+            var id;
+
+            response.stations.forEach(function(stations) {
+                if (stations.type === "Orbis Starport") {
+                    id = stations.id
+                    station.push(id)
+                } else if (stations.type === "Ocellus Starport") {
+                    id = stations.id
+                    station.push(id)
+                } else if (stations.type === "Planetary Port") {
+                    id = stations.id
+                    planetary.push(id)
+                } else if (stations.type === "Outpost") {
+                    id = stations.id
+                    outposts.push(id)
+                }
+            })
+
+            
+            const embed = new RichEmbed()
+            .setColor('RANDOM')
+            .setFooter('EDSM Database')
+            .setTimestamp()
+            .setURL(response.url)
+            .addField('Name', `**${response.name}**` )
+            .addField(`\u200b`,  stripIndents`**Stations** 
+            Rotating Stations: ${station.length}
+            Outposts: ${outposts.length}
+            Planetary Outposts: ${planetary.length}`, true);
+            
+            
+
+            message.channel.send(embed);
+        })
     }
    }
 
