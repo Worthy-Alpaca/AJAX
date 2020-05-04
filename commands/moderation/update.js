28051025a098a869bea7717582e1768cfe2c7b10
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-05-05 00:51:24 +0200

    made bot invisible on join, changed !update

--- commands/moderation/update.js
+++ commands/moderation/update.js
@@ -15,38 +15,32 @@ module.exports = {
             return message.reply("You are not powerfull enough to command me in such a way!").then(m => m.delete(5000));
             
             
-        await client.user.setPresence({
-            status: "invisible",
+        
+        var channel = message.guild.channels.find(channel => channel.name === `${welcome_channel}`);
+
+        
+        client.user.setPresence({
+            status: "online",
             game: {
                 name: `${status}`,
                 type: "WATCHING"
             }
         });
-        var channel = message.guild.channels.find(channel => channel.name === `${welcome_channel}`);
+        if (args[0] === "send") {
+            setTimeout(() => {
+                const embed2 = new RichEmbed()
+                    .setColor("Random")
+                    .setTimestamp()
+                    .setAuthor("Update occured", client.user.displayAvatarURL)
+                    .setDescription(stripIndents`I have been updated. :grin: 
+                    New version: **${version}**`);
 
-        setTimeout(() => {
-            client.user.setPresence({
-                status: "online", 
-                game: {
-                    name: `${status}`,
-                    type: "WATCHING"
-                }
-            });
-
-            
-        }, 5000);
-        
-        setTimeout(() => {
-            const embed2 = new RichEmbed()
-            .setColor("Random")
-            .setTimestamp()
-            .setAuthor("Update occured", client.user.displayAvatarURL)
-            .setDescription(stripIndents`I have been updated. :grin: 
-            New version: **${version}**`);
-
-            return channel.send(embed2);
+                return channel.send(embed2);
  
-        }, 6000)
+            }, 3000)
+        } else return
+
+        
         
 
     }
