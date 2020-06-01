4cf0878c1c574177f450bf639237ef6ca5a705b6
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-06-01 19:22:55 +0200

    playing codecleaner

--- commands/moderation/update.js
+++ commands/moderation/update.js
@@ -20,18 +20,6 @@ module.exports = {
 
         
 
-        exec("update.sh", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-
         client.user.setPresence({
             status: "invisible",
             game: {
