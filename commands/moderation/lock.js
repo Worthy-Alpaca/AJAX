5ef1de8df50f019ea3b24065445d6f52c5435d84
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-06-09 20:01:53 +0200

    removed need to mention a channel when connected

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -1,4 +1,4 @@
-const { admin } = require("../../src/config.json");
+const { getAdmin, getMod } = require("../../functions/functions.js");
 
 module.exports = {
     name: "lock",
@@ -8,18 +8,33 @@ module.exports = {
     run: async (client, message, args, con) => {
         
         message.delete();
+        member = message.member;        
         
         var mID;
         var mID2 = 0;
-        var count = false;      
-
-        if (!args[0]) {
-            return message.reply("You need to tell me what channel to close down")
-        }    
-
+        var count = false;    
+        var channel;
         const mRole = args.slice(0);
-        const a = mRole.pop()
         
+        const a = mRole.pop();
+       
+        var admin = await getAdmin(message, con);
+        var moderator = await getMod(message, con);
+
+        if (admin === null) {
+            return message.channel.send("You need to set the role for admin first. Do that by typing !setadmin")
+        }
+        if (moderator === null) {
+            return message.channel.send("You need to set the role for moderator first. Do that by typing !setmod")
+        }
+
+        if (!message.member.roles.cache.has(message.guild.roles.cache.find(r => r.id=== admin).id)) {
+            if (!message.member.roles.cache.has(message.guild.roles.cache.find(r => r.id=== moderator).id)) {
+                return message.reply("You are not powerfull enough to do that.")
+                    .then(m => m.delete( {timeout: 5000} ));
+            }
+        } 
+
         parseInt(a, 10)
         
         if (Number.isInteger(+a)) {
@@ -28,9 +43,18 @@ module.exports = {
         } else {
             mRole.push(a)            
         }
-        
-        const channel = message.guild.channels.cache.find(channel => channel.name === mRole.join(" "));
 
+        if (!args[0]) {
+            if (member.voice.channel) {
+                channel = message.guild.channels.cache.find(channel => channel.name === member.voice.channel.name);
+            } else {
+                return message.reply("You need to tell me what channel to close down")
+            }            
+        } else {            
+            channel = message.guild.channels.cache.find(channel => channel.name === mRole.join(" "));
+        } 
+
+        
         if (!channel) {
             return message.reply("That channel does not exist")
         }
