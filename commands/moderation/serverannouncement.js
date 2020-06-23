a57014bab980c9bc153deead5d3a119d98f0e5e2
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-06-24 00:47:53 +0200

    improved cross-platform say

--- commands/moderation/serverannouncement.js
+++ commands/moderation/serverannouncement.js
@@ -1,16 +1,17 @@
 const Discord  = require("discord.js");
 const { stripIndents } = require("common-tags");
-const { getservers, getserverchannel } = require("../../functions/functions.js")
+const { getservers, getserverchannel } = require("../../functions/functions.js");
+const { version } = require("../../src/config.json");
 
 module.exports = {
-    name: "serverannouncement",
+    name: "cross-server",
     category: "moderation",
     permission: ["null"],
     description: "Sends a message to all servers",
     usage: "<message>",
     run: async (client, message, args, con) => {
         message.delete();
-
+        //console.log(client.guilds)
         if (message.author.id !== "595341356432621573")
             return message.reply("You are not powerfull enough to command me in such a way!").then(m => m.delete( {timeout: 5000} ));
             
@@ -29,12 +30,12 @@ module.exports = {
         if (args[0] === "list") {  
             srvs = [];
     
-            servers.forEach(server => {
-                var srv = client.guilds.cache.get(server);
-                srvs.push(`${srv.name} => ${srv.id}`)
+            client.guilds.cache.forEach(server => {                
+                srvs.push(`${server.name} => ${server.id}`)
             })            
             
             const embed = new Discord.MessageEmbed()
+                .setTitle("**Servers**")
                 .setTimestamp()
                 .setDescription(stripIndents`- ${srvs.join('\n- ')}`);
 
@@ -42,11 +43,24 @@ module.exports = {
             
         } else if (servers.includes(args[0])) {
             var srv = client.guilds.cache.get(args[0]);
+            if (!srv) {
+                return message.reply("No communication established")
+            }
             channel = srv.channels.cache.find(channel => channel.id === srv.systemChannelID);
             if (!channel) {
                 return message.reply("No communication established")
             }
-            return channel.send(args.slice(1).join(" "))
+
+            const embed = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setFooter(`Version: ${version}`)
+                .setTimestamp()
+                .setTitle("**Greetings**")
+                .setThumbnail(client.user.displayAvatarURL())
+                .setDescription(stripIndents`${args.slice(1).join(" ")}
+                -Worthy Alpaca`);
+
+            return channel.send(embed)
 
         } else {
             asyncForEach(servers, async (server) => {
