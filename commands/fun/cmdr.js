c53958c8e2fec7e8972ad638aee3c135811b72e0
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-06-24 18:06:08 +0200

    hid on !help cmd

--- commands/fun/cmdr.js
+++ commands/fun/cmdr.js
@@ -6,7 +6,7 @@ const { version } = require("../../src/config.json");
 module.exports = {
     name: "cmdr",
     category: "fun",
-    permission: ["none", "moderator", "admin"],
+    permission: ["null"],
     description: "Gets your CMDR stats from inara",
     run: async (client, message, args, con) => {
         if (message.deletable) message.delete();
