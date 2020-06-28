498e2eadde13bb35afcfbd2fa4c0f94e7c5dc59a
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-06-28 16:55:09 +0200

    added commands to add/del ranks

---
+++ commands/info/addrank.js
@@ -0,0 +1,38 @@
+const Discord  = require("discord.js");
+const { getAdmin, setrank } = require("../../functions/functions.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    name: "addrank",    
+    category: "info",
+    permission: ["admin"],
+    description: "Adds a rank to the database",
+    usage: "<rank>",
+    run: async (client, message, args, con) => {
+        if (args < 1) {
+            return message.reply("If you give me some thing to work with, I might be able to help you :wink:").then(m => m.delete({ timeout: 5000 }));
+        }
+        var admin = await getAdmin(message, con);
+        if (!message.member.roles.cache.has(message.guild.roles.cache.find(r => r.id === admin).id)) {
+            return message.reply("You don't have the required permissions to do this.").then(m => m.delete({ timeout: 5000 }));
+        }
+        rank = message.guild.roles.cache.find(r => r.name === args.slice(0).join(" "))
+
+        const embed = new Discord.MessageEmbed()
+
+        if (!rank) {
+            embed.setColor("RED").setDescription("❌ This rank does not exist");
+            return message.channel.send(embed).then(m => m.delete({ timeout: 5000 }));
+        }
+
+        const done = await setrank(message, rank, con);
+
+        if (done) {
+            embed.setColor("GREEN").setDescription("✅ Rank was added successfully.");
+            return message.channel.send(embed).then(m => m.delete({ timeout: 5000 }));
+        } else {
+            embed.setColor("YELLOW").setDescription("❗ This rank already exists for this server");
+            return message.channel.send(embed).then(m => m.delete({ timeout: 5000 }));
+        }
+    }
+}
--- commands/info/rank.js
+++ commands/info/rank.js
@@ -8,8 +8,7 @@ module.exports = {
     name: "rank",
     category: "info",
     permission: ["none", "moderator", "admin"],
-    description: "Applies the given rank.", 
-    descriptionlong: "Applies the given rank. If you have admin rights you can add/del ranks like so: !rank add/del <rank>",
+    description: "Applies the given rank.",     
     usage: "<rank>",   
 
     run: async (client, message, args, con) => {
@@ -17,62 +16,24 @@ module.exports = {
         if (args < 1) {
             return message.reply("If you give me some thing to work with, I might be able to help you :wink:")
         }
-
-        if (args[0] === "add") {
-            var admin = await getAdmin(message, con);
-            if (!message.member.roles.cache.has(message.guild.roles.cache.find(r => r.id=== admin).id)) {
-                return message.reply("You don't have the required permissions to do this.").then(m => m.delete( {timeout: 5000} ));
-            }
-            rank = message.guild.roles.cache.find(r => r.name === args.slice(1).join(" "))
-            if (!rank) {
-                return message.reply("This rank doesn't exist").then(m => m.delete( {timeout: 5000} ));
-            }
-            
-            const done = await setrank(message, rank, con);
-
-            if (done) {
-                return message.reply("Rank was added successfully").then(m => m.delete( {timeout: 5000} ));
-            } else {
-                return message.reply("Rank already exists for this server").then(m => m.delete( {timeout: 5000} ));
-            }
-
-        } else if (args[0] === "del") {
-            var admin = await getAdmin(message, con);
-            if (!message.member.roles.cache.has(message.guild.roles.cache.find(r => r.id=== admin).id)) {
-                return message.reply("You don't have the required permissions to do this.").then(m => m.delete( {timeout: 5000} ));
-            }
-            rank = message.guild.roles.cache.find(r => r.name === args.slice(1).join(" "))
-            if (!rank) {
-                return message.reply("This rank doesn't exist").then(m => m.delete( {timeout: 5000} ));
-            }
-
-            const done = await delrank(message, rank, con)
-
-            if (done) {
-                return message.reply("Rank was deleted successfully").then(m => m.delete( {timeout: 5000} ));
-            } else {
-                return message.reply("This rank doesn't exist in my database").then(m => m.delete( {timeout: 5000} ));
-            }
-        } else {
-            rank = message.guild.roles.cache.find(r => r.name === args.slice(0).join(" "))
-            if (!rank) {
-                return message.reply("This rank doesn't exist").then(m => m.delete( {timeout: 5000} ));
-            }
-
-            const ranktoadd = await getrank(message, rank, con);
-            if (ranktoadd) {
-                if (message.member.roles.cache.has(rank.id)) {
-                    message.member.roles.remove(rank.id).catch(e => console.log(e.message))
-                    message.reply(`\`${rank.name}\` has been taken from you.`)
-                } else {
-                    message.member.roles.add(rank.id).catch(e => console.log(e.message))
-                    message.reply(`\`${rank.name}\` has been added to you.`)
-                }
-                
-            }
+        
+        rank = message.guild.roles.cache.find(r => r.name === args.slice(0).join(" "))
+        if (!rank) {
+            return message.reply("This rank doesn't exist").then(m => m.delete({ timeout: 5000 }));
         }
 
-        
+        const ranktoadd = await getrank(message, rank, con);
+        if (ranktoadd) {
+            if (message.member.roles.cache.has(rank.id)) {
+                message.member.roles.remove(rank.id).catch(e => console.log(e.message))
+                message.reply(`\`${rank.name}\` has been taken from you.`)
+            } else {
+                message.member.roles.add(rank.id).catch(e => console.log(e.message))
+                message.reply(`\`${rank.name}\` has been added to you.`)
+            }
+
+        }
+                
                  
     }
     
