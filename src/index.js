4b08f549f8de477c90fc383d86825832c17f5b3a
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-07-01 20:00:25 +0200

    cleaning code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const { promptMessage, getChnl, getMsg, getapproved, getapproved2, getMember, ge
 const { answers, replies, asks, help, positive, sassy, robot } = require("./answers.json");
 const usersMap = new Map();
 const mysql = require("mysql");
-const {bugs} = require("../package.json")
+const { bugs } = require("../package.json")
 
 
 const client = new Client({
-    disableEveryone: false
+  disableEveryone: false
 });
 
 client.reply = new Collection();
@@ -25,7 +25,7 @@ client.categories = fs.readdirSync("./commands/");
 
 
 ["command"].forEach(handler => {
-    require(`../handler/${handler}`)(client);
+  require(`../handler/${handler}`)(client);
 });
 
 //creating the database connection
@@ -38,49 +38,49 @@ var con = mysql.createConnection({
 });
 
 con.connect(err => {
-  if(err) throw err;
+  if (err) throw err;
   console.log("Connected to Database");
-  con.query("CREATE TABLE IF NOT EXISTS servers(id VARCHAR(20) NOT NULL UNIQUE, name TEXT NOT NULL, admin TEXT, moderator TEXT, greeting VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci, channel TEXT, approved TEXT, startcmd TEXT, reports TEXT, auto_approved TEXT) CHARACTER SET utf8 COLLATE utf8_unicode_ci;")  
+  con.query("CREATE TABLE IF NOT EXISTS servers(id VARCHAR(20) NOT NULL UNIQUE, name TEXT NOT NULL, admin TEXT, moderator TEXT, greeting VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci, channel TEXT, approved TEXT, startcmd TEXT, reports TEXT, auto_approved TEXT) CHARACTER SET utf8 COLLATE utf8_unicode_ci;")
   con.query("CREATE TABLE IF NOT EXISTS ranks(rank_id VARCHAR(20) NOT NULL UNIQUE, server_id VARCHAR(20) NOT NULL, rank_name TEXT NOT NULL);")
 })
 
 client.on("ready", () => {
-    var a = 0;
-    console.log(`Logged in as ${client.user.username}`);
-    
-    client.guilds.cache.forEach(guild => {
-      
-      con.query(`SELECT * FROM servers WHERE id = '${guild.id}'`, (err, rows) => {
-        if(err) throw err;
-        let sql;
-               
-        if (!rows.length) {
-          console.log(guild.name, "added")
-          sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
-          return con.query(sql);
-        }
+  var a = 0;
+  console.log(`Logged in as ${client.user.username}`);
 
-        if(rows[0].id === guild.id) {
-          a++;
-          return;
-        } else {
-          console.log("b")
-          sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
-          con.query(sql);
-        }
-                
-      });
-    })
-    
-    if(a > 0) console.log("No new servers")
+  client.guilds.cache.forEach(guild => {
 
-    client.user.setPresence({
-        status: "online",
-        activity: {
-            name: `${status}`,
-            type: "LISTENING"
-        }
+    con.query(`SELECT * FROM servers WHERE id = '${guild.id}'`, (err, rows) => {
+      if (err) throw err;
+      let sql;
+
+      if (!rows.length) {
+        console.log(guild.name, "added")
+        sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
+        return con.query(sql);
+      }
+
+      if (rows[0].id === guild.id) {
+        a++;
+        return;
+      } else {
+        console.log("b")
+        sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
+        con.query(sql);
+      }
+
     });
+  })
+
+  if (a > 0) console.log("No new servers")
+
+  client.user.setPresence({
+    status: "online",
+    activity: {
+      name: `${status}`,
+      type: "LISTENING"
+    }
+  });
 });
 
 //on joining a new server
@@ -103,38 +103,38 @@ client.on("guildCreate", guild => {
     .addField(`\u200b`, stripIndents`If you have any issues please report them [here.](${bugs.url})`)
 
   //checking for systemmessage channel
-  if (!channel) {    
-      client.users.fetch(message.guild.owner.id, false).then(user => {
-        user.send(embed)
-      })          
+  if (!channel) {
+    client.users.fetch(message.guild.owner.id, false).then(user => {
+      user.send(embed)
+    })
   } else {
     channel.send(embed);
   }
 
-  
+
   con.query(`SELECT * FROM servers WHERE id = '${guild.id}'`, (err, rows) => {
-    if(err) throw err;
+    if (err) throw err;
     let sql;
-    let db;                  
-      
+    let db;
+
     if (!rows.length) {
-        console.log(guild.name, "added")
-        sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
-        return con.query(sql);
+      console.log(guild.name, "added")
+      sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
+      return con.query(sql);
     }
 
-    if(rows[0].id === guild.id) {                  
+    if (rows[0].id === guild.id) {
       db = true;
-      
+
       return;
     } else {
       console.log("b")
       sql = `INSERT INTO servers (id, name) VALUES ('${guild.id}', '${guild.name}')`
       db = true;
-      
+
       con.query(sql);
-    }                          
-            
+    }
+
   });
 })
 
@@ -152,32 +152,32 @@ client.on("guildDelete", guild => {
 
 
 //welcome message
-client.on("guildMemberAdd", async member => {    
-    var greeting;  
-    
-    if (member.bot) return;     
-       
-    greeting = await getMsg(member, con);   
-    bolean = await getautoapproved(member, con);
-    rl = await getapproved(member, con);
-    const role = member.guild.roles.cache.find(r => r.id === rl);
-    
-    if (typeof greeting == 'undefined') {
-      greeting = "Welcome to this generic server. The owner has not bothered with a custom welcome message so you get this one. :person_shrugging:"
-    } else if (greeting === null) {
-      greeting = "Welcome to this generic server. The owner has not bothered with a custom welcome message so you get this one. :person_shrugging:"
-    }      
+client.on("guildMemberAdd", async member => {
+  var greeting;
 
-    if (bolean === "true") {
-      member.roles.add(role.id).catch(e => console.log(e.message));
-    }
-    
-    client.users.fetch(member.id, false).then(user => {
-      return user.send(`Welcome to **${member.guild.name}**. ${greeting}`)
-    })
-    
-    
-        
+  if (member.bot) return;
+
+  greeting = await getMsg(member, con);
+  bolean = await getautoapproved(member, con);
+  rl = await getapproved(member, con);
+  const role = member.guild.roles.cache.find(r => r.id === rl);
+
+  if (typeof greeting == 'undefined') {
+    greeting = "Welcome to this generic server. The owner has not bothered with a custom welcome message so you get this one. :person_shrugging:"
+  } else if (greeting === null) {
+    greeting = "Welcome to this generic server. The owner has not bothered with a custom welcome message so you get this one. :person_shrugging:"
+  }
+
+  if (bolean === "true") {
+    member.roles.add(role.id).catch(e => console.log(e.message));
+  }
+
+  client.users.fetch(member.id, false).then(user => {
+    return user.send(`Welcome to **${member.guild.name}**. ${greeting}`)
+  })
+
+
+
 });
 
 
@@ -185,153 +185,153 @@ client.on("guildMemberAdd", async member => {
 //message handler
 client.on("message", async message => {
 
-    
 
-    if (message.author.bot) return;
-    
-    
-    //automated spam detection and mute
-    if(usersMap.has(message.author.id)) {
-        let mutee = message.member;
-        const reports = await getreportschannel(message, con);
-        const report = message.guild.channels.cache.find(channel => channel.id === reports);
-        const userData = usersMap.get(message.author.id);
-        const { lastMessage, timer } = userData;
-        const difference = message.createdTimestamp - lastMessage.createdTimestamp;
-        let muterole = message.guild.roles.cache.find(r => r.name === "Muted")
 
-        const embed = new Discord.MessageEmbed() 
-            .setColor("#ff0000")
-            .setTimestamp()
-            .setFooter(message.guild.name, message.guild.iconURL)
-            .setAuthor("Muted member", mutee.user.displayAvatarURL())
-            .setDescription(stripIndents`**> Member: ${mutee} (${mutee.id})
+  if (message.author.bot) return;
+
+
+  //automated spam detection and mute
+  if (usersMap.has(message.author.id)) {
+    let mutee = message.member;
+    const reports = await getreportschannel(message, con);
+    const report = message.guild.channels.cache.find(channel => channel.id === reports);
+    const userData = usersMap.get(message.author.id);
+    const { lastMessage, timer } = userData;
+    const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+    let muterole = message.guild.roles.cache.find(r => r.name === "Muted")
+
+    const embed = new Discord.MessageEmbed()
+      .setColor("#ff0000")
+      .setTimestamp()
+      .setFooter(message.guild.name, message.guild.iconURL)
+      .setAuthor("Muted member", mutee.user.displayAvatarURL())
+      .setDescription(stripIndents`**> Member: ${mutee} (${mutee.id})
             **> Automated Mute
             **> Muted in: ${message.channel}
             **> Reason: SPAM
             MUTE needs to be manually removed`);
 
-        
 
-        if(!muterole) {
-            try{
-                muterole = await message.guild.roles.create({
-                    data: {
-                      name: "Muted",
-                      color: "#514f48",
-                      permissions: []
-                    }
-                })
-            message.guild.channels.cache.forEach(async (channel, id) => {              
-                await channel.overwritePermissions([
-                  {
-                    id: muterole.id,
-                    deny: ['SEND_MESSAGES',
-                          'ADD_REACTIONS',
-                          'SEND_TTS_MESSAGES',
-                          'ATTACH_FILES',
-                          'SPEAK']
-                  }
-                ])
-            })
-            } catch(e) {
-            console.log(e.stack);
-            }   
-        }
-        let msgCount = userData.msgCount;
-        
-        if(difference > DIFF) {
-          clearTimeout(timer);
-          console.log('Cleared timeout');
-          userData.msgCount = 1;
-          userData.lastMessage = message;
-          userData.timer = setTimeout(() => {
-            usersMap.delete(message.author.id);
-            console.log('Removed from RESET.');
-          }, TIME);
-          usersMap.set(message.author.id, userData);
-        }
-        else {
-          ++msgCount;
-          if(parseInt(msgCount) === LIMIT) {
-            mutee.roles.add(muterole);
-            message.channel.send(`${mutee} You have been muted. Please contact a staff member to get that reversed.`);
-            report.send(`@here, someone has been auto-muted.`);
-            report.send(embed);
-            /* setTimeout(() => {
-              mutee.removeRole(muterole);
-              message.channel.send('You have been unmuted');
-            }, TIME); */
-          } else {
-            userData.msgCount = msgCount;
-            usersMap.set(message.author.id, userData);
+
+    if (!muterole) {
+      try {
+        muterole = await message.guild.roles.create({
+          data: {
+            name: "Muted",
+            color: "#514f48",
+            permissions: []
           }
-        }
+        })
+        message.guild.channels.cache.forEach(async (channel, id) => {
+          await channel.overwritePermissions([
+            {
+              id: muterole.id,
+              deny: ['SEND_MESSAGES',
+                'ADD_REACTIONS',
+                'SEND_TTS_MESSAGES',
+                'ATTACH_FILES',
+                'SPEAK']
+            }
+          ])
+        })
+      } catch (e) {
+        console.log(e.stack);
       }
-      else {
-        let fn = setTimeout(() => {
-          usersMap.delete(message.author.id);          
-        }, TIME);
-        usersMap.set(message.author.id, {
-          msgCount: 1,
-          lastMessage: message,
-          timer: fn
-        });
+    }
+    let msgCount = userData.msgCount;
+
+    if (difference > DIFF) {
+      clearTimeout(timer);
+      console.log('Cleared timeout');
+      userData.msgCount = 1;
+      userData.lastMessage = message;
+      userData.timer = setTimeout(() => {
+        usersMap.delete(message.author.id);
+        console.log('Removed from RESET.');
+      }, TIME);
+      usersMap.set(message.author.id, userData);
+    }
+    else {
+      ++msgCount;
+      if (parseInt(msgCount) === LIMIT) {
+        mutee.roles.add(muterole);
+        message.channel.send(`${mutee} You have been muted. Please contact a staff member to get that reversed.`);
+        report.send(`@here, someone has been auto-muted.`);
+        report.send(embed);
+        /* setTimeout(() => {
+          mutee.removeRole(muterole);
+          message.channel.send('You have been unmuted');
+        }, TIME); */
+      } else {
+        userData.msgCount = msgCount;
+        usersMap.set(message.author.id, userData);
       }
+    }
+  }
+  else {
+    let fn = setTimeout(() => {
+      usersMap.delete(message.author.id);
+    }, TIME);
+    usersMap.set(message.author.id, {
+      msgCount: 1,
+      lastMessage: message,
+      timer: fn
+    });
+  }
 
-                  
 
-    if (!message.guild) return;
-    
-    //listening for the approved command
-    const startcommand = await getstartcmd(message, con);
 
-    if (message.content.startsWith(`${startcommand}`)) {
-      var chnl;
-      var rl;
-      
-      const member = message.member;
-      guild = member.guild;
-      rl = await getapproved2(message, con);
-      chnl = await getChnl(member, con);
+  if (!message.guild) return;
 
-      const role = message.guild.roles.cache.find(r => r.id === rl);
-      var channel = member.guild.channels.cache.find(channel => channel.id === chnl);  
+  //listening for the approved command
+  const startcommand = await getstartcmd(message, con);
 
-      if (typeof channel == 'undefined') {
-        channel = member.guild.channels.cache.find(channel => channel.id === member.guild.systemChannelID);
-      } else if (channel === null) {
-        channel = member.guild.channels.cache.find(channel => channel.id === member.guild.systemChannelID);
-      }
+  if (message.content.startsWith(`${startcommand}`)) {
+    var chnl;
+    var rl;
 
-      const embed = new Discord.MessageEmbed() 
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setAuthor(`Hooray, ${member.displayName} just joined our merry band of misfits`, member.user.displayAvatarURL())
-      
-      message.member.roles.add(role.id).catch(e => console.log(e.message));
+    const member = message.member;
+    guild = member.guild;
+    rl = await getapproved2(message, con);
+    chnl = await getChnl(member, con);
 
-      return channel.send(embed);
+    const role = message.guild.roles.cache.find(r => r.id === rl);
+    var channel = member.guild.channels.cache.find(channel => channel.id === chnl);
+
+    if (typeof channel == 'undefined') {
+      channel = member.guild.channels.cache.find(channel => channel.id === member.guild.systemChannelID);
+    } else if (channel === null) {
+      channel = member.guild.channels.cache.find(channel => channel.id === member.guild.systemChannelID);
     }
 
-    //command parser
-    if (!message.content.startsWith(prefix)) return;
+    const embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setTimestamp()
+      .setAuthor(`Hooray, ${member.displayName} just joined our merry band of misfits`, member.user.displayAvatarURL())
 
-    if (!message.member) message.member = await message.guild.fetchMember(message);
+    message.member.roles.add(role.id).catch(e => console.log(e.message));
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
+    return channel.send(embed);
+  }
 
-    if (cmd.length === 0) return;
+  //command parser
+  if (!message.content.startsWith(prefix)) return;
 
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
+  if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    if (command)
-        command.run(client, message, args, con);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
 
-   
-    
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command)
+    command.run(client, message, args, con);
+
+
+
 })
 
 
