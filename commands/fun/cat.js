889910a59a0886cd44be703622252a205cbc1a0e
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-09-10 23:13:44 +0200

    restored to active duty

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch');
 module.exports = {
   name: "cat",
   category: "fun",
-  permission: ["null"],
+  permission: ["none", "moderator", "admin"],
   description: "Sends a random cat",
-  descriptionlong: "Sends a random cat. Currently broken due to API being broken.",
+  descriptionlong: "Sends a random cat",
   run: async (client, message, args, con) => {
     if (message.deletable) message.delete();
 
