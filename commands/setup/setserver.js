e87746ffd6b3aac0e153aba1495717de0378c9f4
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-08 22:21:25 +0200

    updated finishing statement

--- commands/setup/setserver.js
+++ commands/setup/setserver.js
@@ -40,7 +40,8 @@ module.exports = {
             .setDescription(stripIndents`You have completed the setup process for this server. :partying_face:
             Hopefully nothing broke :sweat_smile:
             **Important:** For me to work properly you need to put the @AJAX role at the highest possible point in your role list
-            If you wish to change any of this in the future, you can use one of the other commands in the \`setup\` category.`)
+            If you wish to change any of this in the future, you can use one of the other commands in the \`setup\` category.
+            I'll delete the #bot-setup channel in a few minutes.`)
         
         //starting message
         const embed2 = new Discord.MessageEmbed()
