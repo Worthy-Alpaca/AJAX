738a11ddca4bc90df4651fccbec0becc7959f727
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-14 22:52:12 +0200

    added confirmation message

--- commands/administration/gatherstuff.js
+++ commands/administration/gatherstuff.js
@@ -22,9 +22,12 @@ module.exports = {
 
         const check1 = await delete_API_call('commands/delete', payload, payload, 'delete/table');
         const check2 = await delete_API_call('commands/delete', payload2, payload2, 'delete/table');
+        
         if (check1.success === true && check2.success === true) {
             gather_channels(client, post_API_call);
             gather_roles(client, post_API_call);
+
+            message.channel.send("Alright, done.");
         }
     }
 }
