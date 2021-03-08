9501ec7c667da09b9fe0616f316213214e9b6ccb
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-09 00:20:06 +0100

    added function tags to functions

--- functions/default_functions.js
+++ functions/default_functions.js
@@ -4,6 +4,10 @@ const Discord = require('discord.js');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
+    /**
+    * @description handles errors
+    * @param {object} error - the error object
+    */
     error_handler: function (error) {
 
         const embed = new Discord.MessageEmbed()
@@ -17,11 +21,19 @@ module.exports = {
             return user.send(embed);
         });
     },
-
+    /**
+    * @description creates a signed token
+    * @param {sting} ID - the guild ID
+    * @returns {JsonWebKey} token - the token used to authenticate with the API
+    */
     sign_token: function (id) {
         return jwt.sign({ _id: id }, process.env.TOKEN_SECRET);
     },
-
+    /**
+    * @description gathers all channels from all guilds
+    * @param {Discord.Client} client - the client instance
+    * @param {function} post_API_call - the function to create a POST API request
+    */
     gather_channels: function (client, post_API_call) {
         client.guilds.cache.forEach(server => {
             server.channels.cache.forEach(channel => {
@@ -39,7 +51,11 @@ module.exports = {
             })
         })
     },
-
+    /**
+    * @description gathers all roles from all guilds
+    * @param {Discord.Client} client - the client instance
+    * @param {function} post_API_call - the function to create a POST API request
+    */
     gather_roles: function (client, post_API_call) {
         client.guilds.cache.forEach(server => {
             server.roles.cache.forEach(role => {
@@ -52,7 +68,11 @@ module.exports = {
             })
         })
     },
-
+    /**
+    * @description gathers all channels from a guild
+    * @param {Discord.Client} server - the current guild
+    * @param {function} post_API_call - the function to create a POST API request
+    */
     gather_server_channels: function (server, post_API_call) {
         server.channels.cache.forEach(channel => {
 
@@ -68,7 +88,11 @@ module.exports = {
             return post_API_call('channel/create', payload, channel.guild, 'channel');
         })
     },
-
+    /**
+    * @description gathers all roles from a guild
+    * @param {Discord.Client} server - the current guild
+    * @param {function} post_API_call - the function to create a POST API request
+    */
     gather_server_roles: function (server, post_API_call) {
         server.roles.cache.forEach(role => {
             const payload = JSON.stringify({
